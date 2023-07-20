acfb069d355d7aea9fbb16145f00b4636d3ffbe0
jinclaudio <jinclaudio@gmail.com>
2023-07-20 18:19:21 +0200

    sostituzione Vuetify al posto di elements plus

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -2,16 +2,17 @@
 import {ref} from 'vue'
 import axios from "axios";
 
-const text = ref('');
+let text = ref<string>('');
 
 async function postASqueal() {
   axios.post('http://127.0.0.1:3000/users/squeal', {
-    username: "Logged User",
+    username: "keqing",
     text: text,
     category: "general"
   })
       .then((response) => {
         console.log(response.data);
+        console.log(text.value)
       })
       .catch((error) => {
         console.log("Error:", error);
@@ -22,12 +23,12 @@ async function postASqueal() {
 <template>
   <v-textarea
       clearable
+      counter
       label="Label"
       prepend-icon="$vuetify"
       variant="solo-filled"
-      :v-model="text"
+      v-model="text"
   >
-    text
   </v-textarea>
   <v-btn @click="postASqueal">Squeal it</v-btn>
 </template>
